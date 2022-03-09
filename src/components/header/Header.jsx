c369016451e436c2392d753f8e71990c49be282c
Thomas Ehmann <ehmannth@gmail.com>
2022-03-09 21:03:05 +0100

    chore: add header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Search from '../search/Search';
+import './Header.scss';
+
+const Header = () => {
+	return (
+		<div className="header">
+			<Search />
+		</div>
+	);
+};
+
+export default Header;
